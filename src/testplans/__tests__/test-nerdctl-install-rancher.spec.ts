0f9cd699e3289d1325213fd6811f1e7c55195853
Eric Promislow <epromislow@suse.com>
2022-08-11 13:34:53 -0700

    First working version of this test.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/testplans/__tests__/test-nerdctl-install-rancher.spec.ts
+++ src/testplans/__tests__/test-nerdctl-install-rancher.spec.ts
@@ -1,9 +1,12 @@
 import fetch, { Response } from 'node-fetch';
-import retry from 'retry';
-import util from 'util';
+import https from 'https';
+// import retry from 'retry';
+// import util from 'util';
 
 import { tool } from '../../../e2e/utils/TestUtils';
+
 import { ContainerEngine } from '@/config/settings';
+import * as childProcess from '@/utils/childProcess';
 
 describe('Web page test', () => {
   let actualIt = it;
@@ -18,7 +21,7 @@ describe('Web page test', () => {
 
       containerEngine = JSON.parse(stdout).kubernetes.containerEngine;
       expect(containerEngine).not.toBe(ContainerEngine.NONE);
-      [containerClient, otherContainerClient] = containerEngine === ContainerEngine.CONTAINERD ? ['nerdctl', 'moby'] : ['moby', 'nerdctl'];
+      [containerClient, otherContainerClient] = containerEngine === ContainerEngine.CONTAINERD ? ['nerdctl', 'docker'] : ['docker', 'nerdctl'];
     } catch (e) {
       console.error(`Need to run rancher-desktop first`, e);
       actualIt = it.skip;
@@ -38,30 +41,78 @@ describe('Web page test', () => {
   });
 
   it('pushes and tests rancher/rancher', async() => {
-    await tool(containerClient, 'run', '--privileged', '-d', '--restart=no', '-p', '8080:80', '-p', '8443:443', 'rancher/rancher');
-    // Wait up to 300 seconds
-    let i = 0;
-    const limit = 30;
-    let response = new Response();
+    // const imageName: 'nginx'|'rancher/rancher' = 'rancher/rancher';
+    let imageName: 'nginx'|'rancher/rancher' = 'nginx';
+    const expectedString = {
+      nginx: /<title>Welcome to nginx!<\/title>/,
+      'rancher/rancher': /"group":"management.cattle.io"/,
+    }[imageName];
+    const command = `${ containerClient } ps | grep ${ imageName } | awk '{ print $1 }' | xargs -I@ -n 1 bash -c '${ containerClient } stop @ && ${ containerClient } rm @'`;
 
-    for (; i < limit; i++) {
-      try {
-        console.log(`QQQ: >> try ${ i }`);
-        throw new Error(`error ${ i }`);
-        // response = await fetch('http://localhost:8080');
-      } catch (e) {
-        console.log(`QQQ: Failure: ${ e } for try ${ i }`);
-        await util.promisify(setTimeout)(10_000);
-        console.log(`QQQ: + setTimeout`);
-      }
+    // Typescript is annoying
+    if (Math.random() < 0.0000001) {
+      imageName = 'rancher/rancher';
     }
-    expect(i).toBeLessThan(limit);
-    console.log(`QQQ:post-response ${ i }`);
+    if (Math.random() < 0.0000001) {
+      imageName = 'nginx';
+    }
+    try {
+      await childProcess.spawnFile('bash', ['-c', command]);
+    } catch(e: any) {
+      console.log(`Ignoring error ${ e }`);
+    }
+    await tool(containerClient, 'run', '--privileged', '-d', '--restart=no', '-p', '8080:80', '-p', '8443:443', imageName);
+    // Wait up to 600 seconds
+    let i = 0;
+    const limit = 60;
+    let response = new Response();
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
 
-    expect(response.ok).toBeTruthy();
-    expect(response.status).toEqual(200);
+    // Looks like rancher comes up in two steps
+    // First, is the server running?
+    // Then, is it actually doing stuff?
+    while (i < limit) {
+      for (; i < limit; i++) {
+        try {
+          console.log(`QQQ: >> try ${ i }`);
+          // throw new Error(`error ${ i }`);
+          switch (imageName) {
+          case 'nginx':
+            response = await fetch('http://127.0.0.1:8080/index.html');
+            break;
+          case 'rancher/rancher':
+            response = await fetch('https://127.0.0.1:8443/', { agent: httpsAgent });
+            break;
+          }
+          break;
+        } catch (e) {
+          console.log(`QQQ: Failure: ${ e } for try ${ i }`);
+          // For some reason this doesn't work:
+          // await util.promisify(setTimeout)(10_000);
+          await childProcess.spawnFile('sleep', ['10']);
+          console.log(`QQQ: + setTimeout`);
+        }
+      }
+      expect(i).toBeLessThan(limit);
+      console.log(`QQQ:post-response ${ i }`);
+
+      if (!response.ok || response.status >= 400) {
+        console.log(`QQQ: Failure: try ${ i }: ${ response.ok }, status: ${ response.status }`);
+        // await util.promisify(setTimeout)(10_000);
+        await childProcess.spawnFile('sleep', ['10']);
+        console.log(`QQQ: + setTimeout`);
+        i += 1;
+        continue;
+      }
+      break;
+    }
+
+    // expect(response.ok).toBeTruthy();
+    // expect(response.status).toEqual(200);
     console.log(`QQQ:-testing response.text()`);
-    expect(await response.text()).toMatch(/Howdy! Welcome to rancher! Yeehaw!/);
+    expect(await response.text()).toMatch(expectedString);
     console.log(`QQQ:+testing response.text()`);
     // const operation = retry.operation({
     //   retries: 30,
